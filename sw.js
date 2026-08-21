f3494c373e04b8a4218fe41a5e158f2d8f97c64d
Brett  Ross <rossbrett05@gmail.com>
2026-08-20 21:51:50 -0500

    Bump PWA cache to v1.1.0

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE='ff-selector-matrix-v1.0.0';
+const CACHE='ff-selector-matrix-v1.1.0';
 const CORE=['/','/index.html','/manifest.json'];
 self.addEventListener('install',event=>{event.waitUntil(caches.open(CACHE).then(cache=>cache.addAll(CORE)).then(()=>self.skipWaiting()));});
 self.addEventListener('activate',event=>{event.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(key=>key!==CACHE).map(key=>caches.delete(key)))).then(()=>self.clients.claim()));});
