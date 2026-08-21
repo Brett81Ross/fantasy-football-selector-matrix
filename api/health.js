36aef036c5f48398e5b75a208df133b7618504ac
Brett  Ross <rossbrett05@gmail.com>
2026-08-21 04:01:15 -0500

    Update app health metadata for Fantasy Football Matrix v1.3.0

--- api/health.js
+++ api/health.js
@@ -1,7 +1,7 @@
 module.exports = function handler(req, res) {
   res.status(200).json({
     app: 'Fantasy Football Matrix',
-    version: '1.2.1',
+    version: '1.3.0',
     status: 'ok'
   });
 };
